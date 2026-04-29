3fb60d6a9ce43b35201b24c3a6e08f204b64f6aa
brunooliveiradecastro684-maker <brunooliveiradecastro684@gmail.com>
2026-04-29 17:06:22 -0300

    Update info.js

--- info.js
+++ info.js
@@ -1,4 +1,13 @@
-function alterarFundo()
-{
-document.body.style.backgroundColor="#483D8B";
+function adcionarvalores() {
+const formulario = document.getElementById("meuFormulario");
+formulario.addEventListener('submit', function(evento){
+    evento.preventDefault();
+    const tituloD = document.getElementById('titulo').value;
+    const imagemD = document.getElementById('imagem').value
+
+    const novo = { 
+        titulo: tituloD,
+        imagem: imagemD
+    };
+    });
 }
